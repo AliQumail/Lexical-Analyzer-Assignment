abe1d0148a8f1ce5ee71207f2725753e5b7dc0aa
Ali Qumail <aliqumail@gmail.com>
2022-09-18 17:00:43 +0500

    new checks

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 string myText[100] = {};
- 
+string* tokens;
 
 string* tokenize_file_to_string(){
     ifstream file;
@@ -24,10 +24,36 @@ string* tokenize_file_to_string(){
     
 }
 
+void check_valid_comment(){
+
+}
+
+void check_valid_identifier(){
+
+}
+
+void check_valid_string(){
+
+}
+int check_valid_assignment_op(string t){
+    if (t == "="){
+       return 1;
+    } 
+    return 0;
+}
+
+int check_valid_keyword(string t){
+   // returning 1 means true and 0 means false
+   if (t == "int" || t == "if" || t == "else" || t == "do" || t == "until"
+    || t == "then" || t == "read" || t == "display" || t == "displayline" || t == "return" ||
+    t == "function" ){
+        return 1;
+   } 
+   return 0;
+}
+
 int main()
 {
-   string* tokens;
-
    // read file from texts
    // stores it into a string of array to make tokens
    tokens = tokenize_file_to_string();
@@ -39,7 +65,24 @@ int main()
        cout<<tokens[i]<<", ";
     }
 
-    cout<<tokens[0].size();
+    cout<<endl<<endl;
 
+
+    for (int i = 0; i<100 ; i++){
+        if(tokens[i] == "") break;
+        // check if keyword
+        int is_keyword = check_valid_keyword(tokens[i]);
+        if (is_keyword){
+            cout<<tokens[i]<<" is a keyword"<<endl;
+        } else {
+            // check if assignment operator
+            int is_assignment_op = check_valid_assignment_op(tokens[i]);
+            if (is_assignment_op) {
+                cout<<tokens[i]<<" is an assignment operator"<<endl;
+            }
+        }
+    }
+
+    
    return 0;
 }
